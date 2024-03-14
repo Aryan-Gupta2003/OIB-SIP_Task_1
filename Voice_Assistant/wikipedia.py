b68c05181457e150627f2fd0a60398a464b7c2ad
Aryan-Gupta2003 <aryang.gzb@gmail.com>
2024-03-14 12:58:53 +0530

    Made minor changes

--- Voice_Assistant/wikipedia.py
+++ Voice_Assistant/wikipedia.py
@@ -14,7 +14,7 @@ class info():
         search.send_keys(query)
         enter = self.driver.find_element("xpath" ,'//*[@id="search-form"]/fieldset/button')
         enter.click()
-        paragraphs = self.driver.find_elements("xpath", '//*[@id="mw-content-text"]/div[1]/p[2]')
+        paragraphs = self.driver.find_elements("xpath", '//*[@id="mw-content-text"]/div[1]/p[1]')
         text = ""
         for paragraph in paragraphs[:1]:
             text += paragraph.text + "\n"
